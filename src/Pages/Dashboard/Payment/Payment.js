9403e59b9ede0a29012e0da684a6945652b35a6c
saimon1609 <20-44310-3@student.aiub.edu>
2021-12-17 17:55:08 +0600

    first commit

---
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51JyVMdDS79indAiRvbZG3c4EMVNfNTrZPztDy6xKFLTb9aedOjQNfNz1RDwH0xAWOM95A2kVaOBez5eZGZQTfiPQ00K6udnkRl"
+);
+
+const Payment = () => {
+  const { appointmentId } = useParams();
+  const [appointment, setAppointment] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/appointments/${appointmentId}`)
+      .then((res) => res.json())
+      .then((data) => setAppointment(data));
+  }, [appointmentId]);
+  return (
+    <div>
+      <h2>
+        Please Pay for: {appointment.patientName} for {appointment.serviceName}
+      </h2>
+      <h4>Pay: ${appointment.price}</h4>
+      {appointment?.price && (
+        <Elements stripe={stripePromise}>
+          <CheckoutForm appointment={appointment} />
+        </Elements>
+      )}
+    </div>
+  );
+};
+
+export default Payment;
+
+/*
+1. install stripe and stripe-react
+2. set publishable key
+3. Elements
+4. Checkout Form
+-----
+5. Create payment method
+6. server: create payment Intent api
+7. Load client secret
+8. ConfirmCard payment
+9. handle user error
+*/
---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
+// export default firebaseConfig;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2-mf-YWGL1T-qXnRg5Z2vcpzCb2rT1ZU",
+  authDomain: "doctors-portal-48681.firebaseapp.com",
+  projectId: "doctors-portal-48681",
+  storageBucket: "doctors-portal-48681.appspot.com",
+  messagingSenderId: "216942346086",
+  appId: "1:216942346086:web:d66baf6ed1af2672bb52a7",
+};
+
+export default firebaseConfig;
